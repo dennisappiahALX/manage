9cfa1aca1e29473998760673e1bb5e7017055b05
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-06 10:19:48 +0000

    hamburger adjusted

--- build/js/script.js
+++ build/js/script.js
@@ -1,7 +1,7 @@
 const hamburgerBtn = document.getElementById('menu-btn')
 const mobileMenu = document.getElementById('menu')
 
-btn.addEventListener('click', () => {
+hamburgerBtn.addEventListener('click', () => {
   hamburgerBtn.classList.toggle('open')
   mobileMenu.classList.toggle('flex')
   mobileMenu.classList.toggle('hidden')
